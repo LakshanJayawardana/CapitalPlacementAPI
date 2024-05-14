c20a6ecec6b45e71d5b8fc2d5d55ed4718bb9f55
Lakshan Jayawardana <madusaralakshan@gmail.com>
2024-05-14 22:24:48 +0530

    DTOS and Models

--- obj/Debug/net8.0/CapitalPlacementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/CapitalPlacementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapitalPlacementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae290275bed5b59750b96edb2e89934a7ba7f5a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapitalPlacementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapitalPlacementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
